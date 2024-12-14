5a0bb00ec206be0b8703f650d3a03bd0e47c2174
Nehal <65250761+shahnehal0407@users.noreply.github.com>
2024-12-14 23:40:03 +0530

    Time: 241 ms (18.56%) | Memory: 155.8 MB (10.64%) - LeetSync

---
+++ 2868-continuous-subarrays/continuous-subarrays.cpp
@@ -0,0 +1,26 @@
+typedef long long int ll;
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+     int n = nums.size();
+       ll result = 0;
+        
+        multiset<int> all;
+        int r = -1;
+        for (int l = 0; l < n; l ++) {
+            if (l > r) {
+                all.insert(nums[l]);
+                r = l;
+            }
+            
+            while (r < n && (*all.rbegin() - *all.begin()) <= 2) {
+                r ++;
+                if (r < n) all.insert(nums[r]);
+            }
+            result += (r - l);
+            
+            all.erase(all.find(nums[l]));
+        }
+        return result;  
+    } 
+};
