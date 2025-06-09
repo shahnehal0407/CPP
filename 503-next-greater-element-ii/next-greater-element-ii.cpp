cb2fe1709f6304af6015cc7d0d2fee0afab9cc93
Nehal <65250761+shahnehal0407@users.noreply.github.com>
2025-06-10 02:15:38 +0530

    Time: 3 ms (75.55%) | Memory: 29 MB (16.34%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         stack<int> temp;
-        vector<int>ans(nums.size(),-1);
-    for(int i=nums.size()-1;i>=0;i--){
-        temp.push(nums[i]);
-    }
-
-    
-    for(int i=nums.size()-1;i>=0;i--){
-        while(!temp.empty() && nums[i]>=temp.top()){temp.pop();}
-        if(!temp.empty())ans[i]=temp.top();
-        temp.push(nums[i]);
+        int n = nums.size();
+        vector<int> ans(n, -1);
         
-    }
+        // Push the entire array once to simulate circularity
+        for (int i = n - 1; i >= 0; --i) {
+            temp.push(nums[i]);
+        }
+
+        // Main pass
+        for (int i = n - 1; i >= 0; --i) {
+            while (!temp.empty() && temp.top() <= nums[i]) {
+                temp.pop();
+            }
+            if (!temp.empty()) ans[i] = temp.top();
+            temp.push(nums[i]);
+        }
+
         return ans;
     }
 };
