830a14724960f461b60d2c8545a7f3fe89033add
Nehal <65250761+shahnehal0407@users.noreply.github.com>
2025-08-28 14:35:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (15.02%) - LeetSync

---
+++ 233-number-of-digit-one/number-of-digit-one.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countDigitOne(int n) {
+        if(n<=0)return 0;
+        if(n<9)return 1;
+        int ans=0;
+        unordered_map<int,int>mp;
+        mp[9]=1;
+        for(int i=9;i<(INT_MAX-9)/10;i=10*i+9){
+            mp[10*i+9]=mp[i]*10+(i+1);
+
+        }
+
+        int temp=n;
+        int divisor=1;
+        while(temp/10){
+            temp/=10;
+            divisor*=10;
+        }
+        int first_digit=n/divisor;
+        int rem=n%divisor;
+        ans += mp[divisor-1]*first_digit;
+        ans+= (first_digit>1)? divisor:0;
+        ans+=(first_digit==1)? rem+1:0;
+        ans+=countDigitOne(rem);
+        return ans;
+       
+    }
+};
