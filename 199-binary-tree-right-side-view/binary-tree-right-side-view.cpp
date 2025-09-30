a6f89998c23dccdd7d184fd83edbe1350d54095a
Nehal <65250761+shahnehal0407@users.noreply.github.com>
2025-10-01 05:13:40 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (23.69%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -12,36 +12,22 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-                 vector<int> res;
-        if (!root) return res;
+                vector<int>ans;
+        if(!root) return ans;
+        queue<TreeNode*>store;
+        store.push(root);
+        while(!store.empty()){
+            int n=store.size();
 
-        // map<HorizontalDistance, NodeValue>
-        map<int, int> topNode;
-        // queue stores pair: (node, horizontalDistance)
-        queue<pair<TreeNode*, int>> q;
-
-        q.push({root, 0});
-
-        while (!q.empty()) {
-            TreeNode* node = q.front().first;
-            int hd = q.front().second;
-            q.pop();
-
-
-          
-                topNode[hd] = node->val;
-
-
-            if (node->left) q.push({node->left, hd + 1});
-            if (node->right) q.push({node->right, hd + 1});
+            for(int i=0;i<n;i++){
+                TreeNode* node=store.front();store.pop();
+                if(i==n-1)ans.push_back(node->val);
+                if(node->left)store.push(node->left);
+                if(node->right)store.push(node->right);
+            }
         }
 
-        // collect results from leftmost to rightmost hd
-        for (auto it : topNode) {
-            res.push_back(it.second);
-        }
-
-        return res;
+        return ans;
 
 
     }
