1299b999bebe33f3589df576960a1e0870191261
Nehal <65250761+shahnehal0407@users.noreply.github.com>
2026-01-09 18:15:05 +0530

    Time: 2 ms (66.72%) | Memory: 93.8 MB (88.31%) - LeetSync

---
+++ 3189-find-champion-ii/find-champion-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        vector<int>indegree(n,0);
+        for(int i=0;i<edges.size();i++){
+            indegree[edges[i][1]]++;
+        }
+
+        int champ=-1;
+
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+            if(champ!=-1){
+                return -1;
+            }
+            champ=i;
+            }
+            
+        }
+
+        return champ;
+    }
+};
