b9f9979445afc41e0f518b2614eb39ce27baeae5
Nehal <65250761+shahnehal0407@users.noreply.github.com>
2026-05-05 23:42:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (96.89%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -3,8 +3,7 @@ public:
 
     int roblinear(vector<int>& nums,int i,int j){
         int n= nums.size();
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
+
         int prev1=0;
         int prev2=nums[i];
 
@@ -19,6 +18,8 @@ public:
 
     int rob(vector<int>& nums) {
         int n=nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
         return max(roblinear(nums,0,n-2),roblinear(nums,1,n-1));
 
     }
