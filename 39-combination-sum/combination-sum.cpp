ff5a1aa0320e6bada4884a0bf6abee722ce58a0c
Nehal <65250761+shahnehal0407@users.noreply.github.com>
2026-05-23 11:39:17 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (91.88%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
+    void backtrack(vector<int>& k, int t,vector<int>& temp,vector<vector<int>>& ans,int j){
+        if(t==0){ans.push_back(temp);return;}
+        if(t<0)return;
 
-void backtrack(vector<int>& k, int target,vector<int>& temp,int n,vector<vector<int>>& ans,int j){
-    if (target==0) {ans.push_back(temp); return;}
-    if(target<0) return ;
-    for(int i=j;i<n;++i){
-        temp.push_back(k[i]);
-        backtrack(k,target-k[i],temp,n,ans,i);
-        temp.pop_back();
+        for(int i=j;i<k.size();i++){
+            temp.push_back(k[i]);
+            backtrack(k,t-k[i],temp,ans,i);
+            temp.pop_back();
+
+        }
     }
-}
 
 
 
-    vector<vector<int>> combinationSum(vector<int>& k, int target) {
-   int n= k.size();
-    vector<vector<int>> ans;
-    vector<int> temp;
-    
-    backtrack(k,target,temp,n,ans,0);
-    return ans;
 
 
-        
+
+    vector<vector<int>> combinationSum(vector<int>& k, int t) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+
+        backtrack(k,t,temp,ans,0);
+
+        return ans;
     }
 };
