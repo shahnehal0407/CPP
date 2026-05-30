14b299017a9ef03cef98d5644a01616a6423e81d
Nehal <65250761+shahnehal0407@users.noreply.github.com>
2026-05-30 12:48:04 +0530

    Time: 0 ms (100.00%) | Memory: 31.7 MB (58.12%) - LeetSync

--- 4316-minimum-swaps-to-move-zeros-to-end/minimum-swaps-to-move-zeros-to-end.cpp
+++ 4316-minimum-swaps-to-move-zeros-to-end/minimum-swaps-to-move-zeros-to-end.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int minimumSwaps(vector<int>& nums) {
+        int count = 0;
+        int ans = 0;
         int n = nums.size();
 
-        int zeroCount = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                zeroCount++;
-            }
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0) count++;
         }
 
-        int zerosAlreadyAtEnd = 0;
-        for (int i = n - 1; i >= n - zeroCount; i--) {
-            if (nums[i] == 0) {
-                zerosAlreadyAtEnd++;
-            }
+        for(int i = n - 1; i >= n - count; i--){
+            if(nums[i] == 0) ans++;
         }
 
-        return zeroCount - zerosAlreadyAtEnd;
+        return count - ans;
     }
 };
